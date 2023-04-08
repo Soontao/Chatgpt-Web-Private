354a9e41473f6ec1d47b90ca1c45ae9572bc8c10
Theo Sun <theo.sun.github@outlook.com>
2023-04-08 22:12:04 +0800

    fix: auth

--- service/src/middleware/auth.ts
+++ service/src/middleware/auth.ts
@@ -7,6 +7,7 @@ const auth: RequestHandler = async (req, res, next) => {
       const Authorization = req.header('Authorization')
       if (!Authorization || !KEY_LIST.includes(Authorization.substring('Bearer '.length).trim()))
         throw new Error('Error: 无访问权限 | No access rights')
+      next()
     }
     catch (error) {
       res.send({ status: 'Unauthorized', message: error.message ?? 'Please authenticate.', data: null })
